806cde8c23d01a78b322d78cd16553107d9ef754
rmj100 <rmj24@njit.edu>
2021-10-06 11:51:09 -0400

    test lender balance
    test case for checking lender balance

--- src/com/rj/peopleObj/lender.java
+++ src/com/rj/peopleObj/lender.java
@@ -1,5 +1,16 @@
 package com.rj.peopleObj;
 
 public class lender {
+	private double balance;
+
+	public double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+	
+	
 
 }
--- src/test/com/rj/mortgageLender/mortgageLenderTest.java
+++ src/test/com/rj/mortgageLender/mortgageLenderTest.java
@@ -5,15 +5,20 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import com.rj.peopleObj.lender;
+
 class mortgageLenderTest {
+	static lender Lender;
 
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
+		Lender = new lender();
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testCheckLenderBalance() {
+		Lender.setBalance(1000.00);
+		assertTrue (Lender.getBalance() == 1000.00);
 	}
 
 }
